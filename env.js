66b7080286843ed34c3b588821cc6b267cd78910
neo <cyclonejet1@protonmail.com>
2022-06-21 05:29:15 +0545

    template file env.js for config

---
+++ env.js
@@ -0,0 +1,8 @@
+exports.SECRET_KEY = 'some_key';
+
+// Database
+exports.DATABASE = 'bhaka';
+exports.USER = 'bhakasuper';
+exports.PASSWORD = 'bhakasuper';
+exports.PORT = 5432;
+exports.HOST = 'postgres';
