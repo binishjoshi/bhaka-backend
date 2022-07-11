70abc2e39a0e7020745fd93495806e86a65a333d
neo <cyclonejet1@protonmail.com>
2022-07-11 16:03:21 +0545

    added cors bypass

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 initializeDb();
 
 app.listen(5000, () => {
