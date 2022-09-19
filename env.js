6e154d129f270308b1fb98d8c3014ee470a14923
neo <cyclonejet1@protonmail.com>
2022-09-19 15:07:17 +0545

    default configs

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ exports.SECRET_KEY = 'some_key';
 
 // Database
 exports.DATABASE = 'bhaka';
-exports.USER = 'bhakasuper';
+exports.USER = 'bhaka';
 exports.PASSWORD = 'bhakasuper';
 exports.PORT = 5432;
 exports.HOST = 'postgres';
